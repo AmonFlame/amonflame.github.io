1bdb0058cc151be3c9af5bc30629e8a7efb11cdb
AmonFlame <matt.mcb@hotmail.co.uk>
2026-02-28 11:49:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+const firstNumLocation = document.getElementById("first_num");
+const secondNumLocation = document.getElementById("second_num");
+let maxResult = 15;
+let resultMinus = maxResult - 1;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+let answer = getRandomInt(maxResult);
+let firstNum = getRandomInt(resultMinus);
+let secondNum = answer - firstNum;
+firstNumLocation.textContent = firstNum;
+secondNumLocation.textContent = secondNum;
