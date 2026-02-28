aae4204fba629cd78ab9b94f7119ccb89dab0b75
AmonFlame <matt.mcb@hotmail.co.uk>
2026-02-28 12:42:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const firstNumLocation = document.getElementById("first_num");
 const secondNumLocation = document.getElementById("second_num");
 const resultLocation = document.getElementById("result");
 const check = document.getElementById("check");
+const blockArea1 = document.getElementById("block_area1");
+const blockArea2 = document.getElementById("block_area2");
 
 let maxResult = 15;
 let answer;
+let firstNum;
+let secondNum;
 
 function getRandomInt(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -12,8 +16,8 @@ function getRandomInt(max, min) {
 
 function generateQuestion() {
   answer = getRandomInt(maxResult, 5);
-  let firstNum = getRandomInt(answer + 1, 1);
-  let secondNum = answer - firstNum;
+  firstNum = getRandomInt(answer + 1, 1);
+  secondNum = answer - firstNum;
 
   firstNumLocation.textContent = firstNum;
   secondNumLocation.textContent = secondNum;
@@ -21,18 +25,9 @@ function generateQuestion() {
   generateBlocks();
 }
 
-generateQuestion(); // initial question
-
-const blockArea1 = document.getElementById("block_area1");
-const blockArea2 = document.getElementById("block_area2");
-
 function generateBlocks() {
-
   blockArea1.innerHTML = "";
   blockArea2.innerHTML = "";
-
-  const firstNum = Number(firstNumLocation.textContent);
-  const secondNum = Number(secondNumLocation.textContent);
 
   for (let i = 0; i < firstNum; i++) {
     const newDiv = document.createElement("div");
@@ -47,13 +42,13 @@ function generateBlocks() {
   }
 }
 
+generateQuestion();
 
 check.addEventListener("click", () => {
   if (Number(resultLocation.value) === answer) {
     alert("You did it!");
-    
-    resultLocation.value = "";   // clear input
-    generateQuestion();          // new numbers
+    resultLocation.value = "";
+    generateQuestion();
   } else {
     alert("Try again!");
   }
