312d4ade01fb03fe1ae8e8960619fa7d6ff1b113
AmonFlame <matt.mcb@hotmail.co.uk>
2026-02-28 12:16:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const firstNumLocation = document.getElementById("first_num");
 const secondNumLocation = document.getElementById("second_num");
+const resultLocation = document.getElementById("result");
+const check = document.getElementById("check");
+
 let maxResult = 15;
+let answer;
+
 function getRandomInt(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-let answer = getRandomInt(maxResult, 0);
-let firstNum = getRandomInt(answer + 1, 0);
-let secondNum = answer - firstNum;
-firstNumLocation.textContent = firstNum;
-secondNumLocation.textContent = secondNum;
-const resultLocation = document.getElementById("result");
-const check = document.getElementById("check");
+
+function generateQuestion() {
+  answer = getRandomInt(maxResult, 0);
+  let firstNum = getRandomInt(answer + 1, 0);
+  let secondNum = answer - firstNum;
+
+  firstNumLocation.textContent = firstNum;
+  secondNumLocation.textContent = secondNum;
+}
+
+generateQuestion(); // initial question
+
 check.addEventListener("click", () => {
-if (Number(resultLocation.value) === answer) {
-  alert("You did it!");
-} else {
-  alert("Try again!");
-}
-  
-}
-);
+  if (Number(resultLocation.value) === answer) {
+    alert("You did it!");
+    
+    resultLocation.value = "";   // clear input
+    generateQuestion();          // new numbers
+  } else {
+    alert("Try again!");
+  }
+});
