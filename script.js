0e41c2247027a32af111d8ba480855c17454a78d
AmonFlame <matt.mcb@hotmail.co.uk>
2026-02-28 12:02:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const firstNumLocation = document.getElementById("first_num");
 const secondNumLocation = document.getElementById("second_num");
 let maxResult = 15;
-let resultMinus = maxResult - 1;
 function getRandomInt(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 let answer = getRandomInt(maxResult, 0);
-let firstNum = getRandomInt(resultMinus, 0);
+let firstNum = getRandomInt(answer + 1, 0);
 let secondNum = answer - firstNum;
 firstNumLocation.textContent = firstNum;
 secondNumLocation.textContent = secondNum;
+const resultLocation = document.getElementById("result");
+const check = document.getElementById("check");
+check.addEventListener("click", () => {
+if (resultLocation.value === answer) {
+  alert("You did it!");
+} else {
+  alert("Try again!");
+}
+  
+};
+);
